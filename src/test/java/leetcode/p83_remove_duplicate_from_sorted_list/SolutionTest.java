1ba5726a32b84016d12132b9ccf94810ff8e3129
thien <thien.ntt2205@gmail.com>
2022-01-20 16:28:54 +0700

    Add design_linked_list

---
+++ src/test/java/leetcode/p83_remove_duplicate_from_sorted_list/SolutionTest.java
@@ -0,0 +1,12 @@
+package leetcode.p83_remove_duplicate_from_sorted_list;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    void deleteDuplicates() {
+    }
+}
